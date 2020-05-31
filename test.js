1d8b0859c568a45f4da215daf9ea089e6933e27a
Wing <Wing@163.com>
2020-05-31 21:12:35 +0800

    update

--- test.js
+++ test.js
@@ -4,10 +4,19 @@ console.log(txt)
 
 console.log('the latest new context')
 
-最新
+// 最新
 
-工作区是编辑器中的内容
+// 工作区是编辑器中的内容
 
 // 我们看看 git 是怎么处理冲突呢
 
 // github edit
+
+
+function add(num) {
+  return num + 2
+}
+
+function warn(txt) {
+  console.log(txt)
+}
